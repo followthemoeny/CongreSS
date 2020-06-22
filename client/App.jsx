d31e054f0194da53ece584e9616757210d190c6a
LolDragoon <Alex_Havas@hotmail.com>
2020-06-22 17:09:06 -0400

    Boiler plate made

---
+++ client/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const App = () => {
+  return (<div>greetings</div>)
+}
+
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const mode = process.env.NODE_ENV
+const webpack = require('webpack')
+module.exports = {
+    devServer: {
+      publicPath: '/build/',
+      proxy: {
+         '/api': 'http://localhost:3000',
+      },
+      port: 8080,
+      hot: true,
+    },
+    entry: ['./client/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: 'http://localhost:8080/build/'
+    },
+    mode,
+    plugins: [ new webpack.HotModuleReplacementPlugin() ],
+    module: {
+        rules: [
+             { 
+              test: /\.jsx?/,
+              exclude: /node_modules/,
+              use: {
+              loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env','@babel/preset-react']
+                } 
+              }
+            },
+        ]
+    }
+  };
