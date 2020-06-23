3a65e760985b38ff496dfd71e9914cc3113ad9fd
a-straus <a.straus1@gmail.com>
2020-06-23 15:02:59 -0400

    created Global Style

---
+++ client/components/style/globalStyle.js
@@ -0,0 +1,6 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Eczar:wght@800&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;1,200;1,300;1,400&display=swap');
+`;
