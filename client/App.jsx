43d0c1419d305bd6e7fa0884e4b36baa961ffc41
Madison Brown <mbrown3391@gmail.com>
2020-06-22 21:47:43 -0400

    404 page

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import ProtectedRoute from './util/ProtectedRoute.jsx';
 
 import { SessionContext } from './contexts/Session.jsx';
@@ -14,22 +14,25 @@ const App = () => {
 
   const validate = () => {
     if (!session.address) {
-      return "/";
+      return '/';
     }
   };
 
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
+        <Route exact path='/'>
           <Portal />
         </Route>
-        <ProtectedRoute path="/officials" validate={validate}>
+        <ProtectedRoute path='/officials' validate={validate}>
           <Officials />
         </ProtectedRoute>
-        <ProtectedRoute path="/elections" validate={validate}>
+        <ProtectedRoute path='/elections' validate={validate}>
           <Elections />
         </ProtectedRoute>
+        <Route>
+          <h1>404 Not Found</h1>
+        </Route>
       </Switch>
     </Router>
   );
